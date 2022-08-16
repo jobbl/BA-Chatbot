1d04bb801ea09d7afbf466d318c1927614c29880
Jordan Richter <62934673+jobbl@users.noreply.github.com>
2022-08-16 10:05:08 +0200

    docker

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ import os
 wav_question = "question.wav"
 wav_response = "response.wav"
 
+intro = "Welcome! You have found your way to an experiment about an emotion understanding therapy bot. At least thats the aim ^^. \n First, The bot will guide you through a little conversation of about 5 minutes. At the end it will ask you to follow a link to a short survey, which also takes about 5 minutes. Everything is totally anonymous. The things you say during the session are not saved, and the survey is anonymous. \n When you are ready, make sure you are not muted and then press on the \"Record\" Button. The browser will ask you for microphone permissions, if have not given them already. When you are done speaking, the bot will take a while to process and then answer you. Have fun!   "
+
 # create Flask instance
 app = Flask(__name__)
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
 
 
 # Whenever there is a request from the client to this url, run the following code
@@ -68,7 +74,7 @@ def extract():
         return render_template('index.html', result=response, text=text)
     
     else:
-        return render_template('index.html')
+        return render_template('index.html',intro = intro)
 
 @app.route('/audio_response/<variable>', methods=['POST', 'GET'])
 def download(variable):
