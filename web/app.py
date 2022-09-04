ad591e811d241bfcb7c239cdeb02851f3ba8a6f6
Jordan Richter <derquerbaer@gmail.com>
2022-09-04 19:19:52 +0200

    change heroku.yml

--- web/app.py
+++ web/app.py
@@ -188,4 +188,4 @@ def restart():
 
 # executes when script is called -> starts the server, takes optional arguments like port number and debugging amount, see flask documentation
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0',port=5000,debug=True)
+    app.run(host= '0.0.0.0',port=80,debug=True)
